7d94ba9a5ee71b9501c8288aafcead4473fbc811
nirajandhungel <subashdhungel555@gmail.com>
2025-08-11 23:50:52 +0545

    added string and used var in cout

--- test.cpp
+++ test.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
 int main() {
-    cout << "Hello, world!"<<endl;// endl does two things: Adds a newline Flushes the output buffer immediately using cout << flush;
+    
+    int age = 19;
+    string name = "Nirajan";
+    cout << "Hello, My name is "<<name <<" and I am " << age<<"years old." <<endl;// endl does two things: Adds a newline Flushes the output buffer immediately using cout << flush;
     return 0;
 }
