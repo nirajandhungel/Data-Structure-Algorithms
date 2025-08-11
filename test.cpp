bdd0caa37a3e40879422ebeee9963f35b45e9cff
nirajandhungel <subashdhungel555@gmail.com>
2025-08-11 23:42:13 +0545

    added endl, flushing output

--- test.cpp
+++ test.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, world!";
+    cout << "Hello, world!"<<endl;// endl does two things: Adds a newline Flushes the output buffer immediately using cout << flush;
     return 0;
 }
