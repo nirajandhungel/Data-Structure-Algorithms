d7371ca0d15b1d3831dba450d139bde223aa1187
nirajandhungel <subashdhungel555@gmail.com>
2025-08-12 22:39:02 +0545

    Ternary operatt in cpp

---
+++ TernaryOper/ternaryOpp.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main (){
+    int age = 19;
+    string statement = (age>18)?"You Can Drive":"You cant Drive";
+    cout<<endl<<statement<<endl;
+    return 0 ;
+}
