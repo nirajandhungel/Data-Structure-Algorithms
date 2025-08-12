c9f28595e94ba31449e6f9aee5609b73a6ec76ab
nirajandhungel <subashdhungel555@gmail.com>
2025-08-13 00:31:19 +0545

    Triangle Pattern

---
+++ StarPattern/triangle.cpp
@@ -0,0 +1,14 @@
+// Wap to make traingle of stars
+
+#include<iostream>
+using namespace std;
+int main (){
+    for(int i =0;i<4;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+
+        cout<<endl;
+    }
+    return 0;
+}
