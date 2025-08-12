3d4b5d698ddebf5ae82e95839f07c577c404ecb7
nirajandhungel <subashdhungel555@gmail.com>
2025-08-13 00:26:48 +0545

    Square Pattern

---
+++ StarPattern/square.cpp
@@ -0,0 +1,14 @@
+// Wap to display square pattern
+
+#include<iostream>
+using namespace std;
+int main (){
+    int length = 4;
+    for(int i =0;i<length;i++){
+        for(int j=0;j<length;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
