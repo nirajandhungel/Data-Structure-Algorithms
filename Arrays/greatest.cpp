356a3925351e44ec16ee8c8230de2c97002d4de4
nirajandhungel <subashdhungel555@gmail.com>
2025-08-16 23:13:52 +0545

    Greatest elem in an array

---
+++ Arrays/greatest.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num [5] ={4,1,8,2,3};
+    int greatest;
+    for(int i=0;i<5 ;i++){
+        for(int j=i; j<5;j++){
+            if(num[i]>num[j]){
+                greatest = num[i];
+
+            }
+        }
+    }
+    cout<<greatest<<endl;
+    return 0;
+}
