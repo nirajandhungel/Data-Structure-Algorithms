f5d0e60705d0e2cdc227161359a463a887931c01
nirajandhungel <subashdhungel555@gmail.com>
2025-08-16 23:07:40 +0545

    Array in C++

---
+++ Arrays/arrayMemory.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int num [5]={1,2,3,4,5};
+    cout<<num<<endl;
+    cout<<&num[0]<<endl;
+    cout<<&num[1]<<endl;
+    cout<<&num[2]<<endl;
+    cout<<&num[3]<<endl;
+    return 0;
+}
