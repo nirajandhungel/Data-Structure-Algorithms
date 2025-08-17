c7bb5a484ff26edbedd9092db6428e56e6f3d3fd
nirajandhungel <subashdhungel555@gmail.com>
2025-08-17 22:37:45 +0545

    Linear 2-way serach for a target in an array

--- Algorithms/linearSerach.cpp
+++ Algorithms/linearSerach.cpp
@@ -1,2 +1,21 @@
 #include<iostream>
-using names
+using namespace std;
+int linearSearch(int arr[], int length ,int target);
+int main (){
+    int arr [6] = {2,6,1,8,9,3};
+    int result = linearSearch(arr,6,5);
+    cout<<result<<endl;
+    return 0;
+}
+
+int linearSearch(int arr[], int length ,int target){  // Two way search faster from both sides
+    int i=0, j=length-1;
+    while(i<=j){
+        if(arr[i]==target) return i;
+        if(arr[j]==target) return j;
+        i++;j--;
+    }
+    return -1; // if not found, worst time complexity
+
+
+}
