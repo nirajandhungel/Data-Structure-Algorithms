6da3ba58a0c3df0b78568a76bcdce97fd1c9365c
nirajandhungel <subashdhungel555@gmail.com>
2025-08-17 17:53:35 +0545

    Smallest value finder using climits INT_MAX

---
+++ Arrays/smallest.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+int main(){
+    int smallest = INT_MAX;
+    int arr [6] = {6,1,-9,7,8,-2};
+    for(int i =0; i<6;i++){
+        if(arr[i]<smallest){
+            smallest = arr[i];
+        }
+    }
+    cout<<smallest<<endl;
+
+    // int minVal = INT_MIN;
+
+
+    return 0;
+}
