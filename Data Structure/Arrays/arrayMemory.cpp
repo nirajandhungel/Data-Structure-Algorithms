639994aca2a0b25d8639a702bdae124a6f3dad47
nirajandhungel <subashdhungel555@gmail.com>
2025-08-17 23:24:49 +0545

    WAP to print memory address of array elements

--- Data Structure/Arrays/arrayMemory.cpp
+++ Data Structure/Arrays/arrayMemory.cpp
@@ -1,11 +1,13 @@
+// WAP to print memory address of array elements
+
 #include<iostream>
 using namespace std;
 int main (){
     int num [5]={1,2,3,4,5};
     cout<<num<<endl;
-    cout<<&num[0]<<endl;
-    cout<<&num[1]<<endl;
-    cout<<&num[2]<<endl;
-    cout<<&num[3]<<endl;
+    cout<<&num[0]<<endl;  //memory address of first element // base address
+    cout<<&num[1]<<endl; //memory address of second element
+    cout<<&num[2]<<endl; //memory address of third element
+    cout<<&num[3]<<endl; //memory address of fourth element
     return 0;
 }
