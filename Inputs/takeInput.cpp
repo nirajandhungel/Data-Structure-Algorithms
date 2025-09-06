928fb17dd5a9af910c9895c31bf33ff2975b621a
nirajandhungel <subashdhungel555@gmail.com>
2025-09-06 20:15:33 +0545

    input: wap to take input

---
+++ Inputs/takeInput.cpp
@@ -0,0 +1,13 @@
+// WAP to take input from user
+
+#include<iostream>
+using namespace std;
+int main (){
+
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    cout<<"Yoy entered: "<<num<<endl;
+    return 0;
+}
